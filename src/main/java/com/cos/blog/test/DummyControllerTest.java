3b08d5743cf915e6cf99c697ce1c59835dbd2569
Eunho Moon <eunho.moon@gmail.com>
2022-01-19 23:11:36 +0900

    dummy data를 이용한 select

--- src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -1,6 +1,10 @@
 package com.cos.blog.test;
 
+import java.util.function.Supplier;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,11 +12,35 @@ import com.cos.blog.model.RoleType;
 import com.cos.blog.model.User;
 import com.cos.blog.repository.UserRepository;
 
-@RestController
+@RestController	// html이 아닌 data를 리턴해주는 controller
 public class DummyControllerTest {
 	
 	@Autowired	// DummyControllerTest가 메모리에 뜰 때 @Autowired로 연결된 객체도 같이 뜬다.(의존성 주입)
 	private UserRepository userRepository;
+	
+	// {id} 주소로 파라미터를 전달받을 수 있다.
+	@GetMapping("/dummy/user/{id}")
+	public User detail(@PathVariable int id) {
+		
+		User user = userRepository.findById(id).orElseThrow(new Supplier<IllegalArgumentException>() {
+
+			@Override
+			public IllegalArgumentException get() {
+				return new IllegalArgumentException("해당 유저는 존재하지 않습니다. id : " + id);
+			}
+			
+		});
+		// 찾는 값이 DB에 없을 경우 null값이 리턴되기 때문에 Optional로 User객체를 감싸서 가져온다.
+	
+		// 람다식
+//		User user = userRepository.findById(id).orElseThrow(() -> {
+//			return new IllegalArgumentException("해당 유저는 존재하지 않습니다. id : " + id);
+//		});
+		
+		// 요청 = 웹 브라우저 / user 객체 = Java Object 
+		// 웹 브라우저가 이해할 수 있는 데이터로 변환(json)
+		return user;
+	}
 
 	// http의 body에 있는 username, password, email 데이터를 가지고 요청
 	@PostMapping("/dummy/join")
