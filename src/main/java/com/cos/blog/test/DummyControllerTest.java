e7a86cc6c9666740f402cffba95645fa72cd1a93
Eunho Moon <eunho.moon@gmail.com>
2022-01-22 23:33:15 +0900

    업데이트

--- src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -1,11 +1,20 @@
 package com.cos.blog.test;
 
+import java.util.List;
 import java.util.function.Supplier;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cos.blog.model.RoleType;
@@ -17,6 +26,42 @@ public class DummyControllerTest {
 	
 	@Autowired	// DummyControllerTest가 메모리에 뜰 때 @Autowired로 연결된 객체도 같이 뜬다.(의존성 주입)
 	private UserRepository userRepository;
+	
+	// save 함수는 id를 전달하지 않으면 insert를 해주고, id를 전달했을 때 해당 id가 있으면 update, 없으면 insert를 해준다.
+	@Transactional
+	@PutMapping("/dummy/user/{id}")
+	public String updateUser(@PathVariable int id, @RequestBody User requestUser) {
+	// @RequestBody : json 데이터를 요청하면 Java Object로 변환(MessageConverter의 Jackson 라이브러리)해서 받아준다.
+		System.out.println("id : " + id);
+		System.out.println("password : " + requestUser.getPassword());
+		System.out.println("email : " + requestUser.getEmail());
+		
+		User user = userRepository.findById(id).orElseThrow(() -> {
+			return new IllegalArgumentException("수정에 실패하였습니다.");
+		});
+		user.setPassword(requestUser.getPassword());
+		user.setEmail(requestUser.getEmail());
+		// null 값이 들어가지 않도록 id를 이용해 데이터를 담아와서 변경한다.(null 값이 없는 꽉 찬 데이터)
+		
+		//	userRepository.save(user);		// 업데이트 할 때는 save를 잘 사용하지 않는다.(null 값이 들어갈 수 있기 때문)
+		// 더티 체킹 : 
+		return "업데이트에 성공했습니다.";
+	}
+	
+	@GetMapping("/dummy/users")
+	public List<User> list() {
+		return userRepository.findAll();
+	}
+	
+	// 한 페이지당 두 건의 데이터를 리턴받는다.
+	@GetMapping("/dummy/user")
+	public List<User> pageList(@PageableDefault(size = 2, sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
+		Page<User> pagingUser = userRepository.findAll(pageable);
+		
+		List<User> users = pagingUser.getContent();
+		// getContent() :  Json 데이터만 List 형태로 리턴
+		return users;
+	}
 	
 	// {id} 주소로 파라미터를 전달받을 수 있다.
 	@GetMapping("/dummy/user/{id}")
